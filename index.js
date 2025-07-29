43ec36810c398abbea3f2eea778bd6c2df7d1c37
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-07-29 06:14:16 +0300

    write a file

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.readFile(
-  path.join(__dirname, "01-TUT", "lorem.txt"),
-  "utf-8",
-  (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(data);
-    }
+fs.readFile(path.join(__dirname, "lorem.txt"), "utf-8", (err, data) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(data);
   }
-);
+});
 
 process.on("uncaughtException", (err) => {
   console.log("there was an uncaught error");
   process.exit(1);
 });
+
+fs.writeFile(path.join(__dirname, "hi.txt"), "hello node", (err) =>
+  console.err(err)
+);
