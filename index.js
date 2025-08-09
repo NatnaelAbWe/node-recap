a0494154cf2ec90e5a005f27545875824e717a95
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-08-09 18:08:07 +0300

    emit events

--- index.js
+++ index.js
@@ -1,72 +1,12 @@
-// working with a file system module
+const logEvents = require("./logEvent");
 
-// const fsPromises = require("fs").promises;
-// const path = require("path");
+const EventEmmiter = require("events");
 
-// // fs.readFile(path.join(__dirname, "lorem.txt"), "utf-8", (err, data) => {
-// //   if (err) {
-// //     throw err;
-// //   } else {
-// //     console.log(data);
-// //   }
-// // });
+class MyEmitter extends EventEmmiter {}
+const myEmmitter = new MyEmitter();
 
-// // process.on("uncaughtException", (err) => {
-// //   console.log("there was an uncaught error");
-// //   process.exit(1);
-// // });
-
-// // fs.writeFile(path.join(__dirname, "hi.txt"), "hello node", (err) =>
-// //   console.error(err)
-// // );
-
-// // fs.appendFile(
-// //   path.join(__dirname, "hi.txt"),
-// //   "\n\n i am recaping node",
-// //   (err) => console.log(err)
-// // );
-
-// // fs.rename(path.join(__dirname, "hi.txt"), "hello.txt", (err) =>
-// //   console.error(err)
-// // );
-
-// const fileOps = async () => {
-//   try {
-//     const data = await fsPromises.unlink(
-//       path.join(__dirname, "starter.txt"),
-//       "utf-8"
-//     );
-//     console.log(data);
-//     console.log("reading");
-//     await fsPromises.writeFile(
-//       path.join(__dirname, "abebe.txt"),
-//       "hi i am abebe"
-//     );
-//     console.log("writting");
-//     await fsPromises.appendFile(
-//       path.join(__dirname, "abebe.txt"),
-//       "hi lulu zulu"
-//     );
-//     console.log("appending");
-//     await fsPromises.rename(
-//       path.join(__dirname, "abebe.txt"),
-//       path.join(__dirname, "next.txt")
-//     );
-//   } catch (err) {
-//     console.error(err);
-//   }
-// };
-
-// fileOps();
-
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
-
-console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
-
-console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
-
-console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
-
-console.log("hell0");
-console.log(uuid());
+myEmmitter.on("log", (msg) => logEvents(msg));
+setTimeout(() => {
+  // emit the event
+  myEmmitter.emit("log", "event emmited");
+}, 2500);
---
+++ logEvent.js
@@ -0,0 +1,25 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message) => {
+  const dateTime = format(new Date(), "yyyyMMdd\thh:mm:ss");
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+
+  try {
+    if (!fs.existsSync(path.join(__dirname, "Logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "Logs"));
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "Logs", "eventLog.txt"),
+      logItem
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = logEvents;
